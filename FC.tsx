9329258e66a401b4ec262ad917b8b6473649f972
931923959 <931923959@qq.com>
2019-08-28 17:02:48 +0800

    添加FC

---
+++ FC.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import './index.css'
+
+interface Props {
+
+};
+export const ComponentName: React.FC<Props> = props => {
+  const {  } = props;
+  return (
+    <div className="">
+
+    </div>
+  )
+};
