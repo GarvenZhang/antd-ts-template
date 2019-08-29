caa688710cbd336236b5b28c03e3218cc8a66171
931923959 <931923959@qq.com>
2019-08-29 19:43:16 +0800

    新增class component的模板

---
+++ CC.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { connect } from 'react-redux'
+import {  } from 'antd'
+
+import "./index.scss";
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+let ComponentName: any = class extends React.Component<
+  Props,
+  State
+  > {
+  readonly state: State = {
+
+  };
+
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    const {
+
+    } = this;
+    const {
+
+    } = this.props;
+    const {
+
+    } = this.state;
+
+    return (
+      <div className="">
+
+      </div>
+    )
+  }
+};
+
+ComponentName = connect(
+  (state: any) => ({
+
+  }),
+  {
+    ...{
+
+    }
+  }
+)(ComponentName);
+
+export { ComponentName };
